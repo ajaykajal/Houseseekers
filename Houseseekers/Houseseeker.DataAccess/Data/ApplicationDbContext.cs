640b72de3a986ccfef99535f8d46e6a0b0e755cd
ajaykumar <aks7879@gmail.com>
2022-11-01 12:08:23 +0530

    Implemented archtecture of the houseseekers API

---
+++ Houseseekers/Houseseeker.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Houseseekers.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Houseseeker.DataAccess.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
